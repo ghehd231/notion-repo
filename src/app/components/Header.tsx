0c74cccf053247e02dc3796133e4eb1f51095c3a
ghehd231 <45757382+ghehd231@users.noreply.github.com>
2023-12-01 11:27:17 +0900

    feat: update navigation cursor on header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,10 +8,18 @@ const Header = () => {
     <Flex wrap="wrap" justify="between">
       <Tabs.Root defaultValue="profile">
         <Tabs.List>
-          <Tabs.Trigger value="profile" onClick={() => router.push("/")}>
+          <Tabs.Trigger
+            value="profile"
+            onClick={() => router.push("/")}
+            style={{ cursor: "pointer" }}
+          >
             <Text size="3">Profile</Text>
           </Tabs.Trigger>
-          <Tabs.Trigger value="notion" onClick={() => router.push("/notion")}>
+          <Tabs.Trigger
+            value="notion"
+            onClick={() => router.push("/notion")}
+            style={{ cursor: "pointer" }}
+          >
             <Text size="3">Notion Blog</Text>
           </Tabs.Trigger>
         </Tabs.List>
