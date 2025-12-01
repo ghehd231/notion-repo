17b80f0f7bb288d1714afc1919df46a820769ceb
ghehd231 <45757382+ghehd231@users.noreply.github.com>
2025-12-01 13:17:26 +0900

    refactor: apply react_cache to getting notion page

--- src/app/lib/notions.ts
+++ src/app/lib/notions.ts
@@ -1,4 +1,13 @@
+import { cache } from "react";
 import { NotionAPI } from "notion-client";
 
 const notion = new NotionAPI();
+
+//react cache 적용하여 한번만 호출되게 수정
+//https://nextjs-ko.org/docs/app/building-your-application/caching#react-cache-function
+export const getPage = cache(async (id: string) => {
+  const page = await notion.getPage(id);
+  return page;
+});
+
 export default notion;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import { getPageTitle } from "notion-utils";
 import { commonTitle } from "./data/metaTitle";
 
 import { rootNotionPageId } from "./lib/config";
-import notion from "./lib/notions";
+import { getPage } from "./lib/notions";
 
 import NotionPage from "./components/NotionPage";
 
 export async function generateMetadata(): Promise<Metadata> {
-  const recordMap = await notion.getPage(rootNotionPageId);
+  const recordMap = await getPage(rootNotionPageId);
   const title = getPageTitle(recordMap);
 
   return {
@@ -22,7 +22,7 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function Home() {
-  const recordMap = await notion.getPage(rootNotionPageId); //getting notion data on Server Side
+  const recordMap = await getPage(rootNotionPageId); //getting notion data on Server Side
 
   return (
     <main>
